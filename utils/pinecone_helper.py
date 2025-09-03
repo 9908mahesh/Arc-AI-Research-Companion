e1552a8624246629805c79acdc5f320db8d500c2
9908mahesh <maahimahi9908@gmail.com>
2025-09-04 02:29:41 +0530

    Update pinecone_helper.py

--- utils/pinecone_helper.py
+++ utils/pinecone_helper.py
@@ -1,10 +1,21 @@
 import pinecone
-from langchain.vectorstores import Pinecone
 from langchain.embeddings import OpenAIEmbeddings
-from config import OPENAI_API_KEY, PINECONE_API_KEY, PINECONE_ENVIRONMENT, PINECONE_INDEX_NAME
+from langchain.vectorstores import Pinecone as LangPinecone
+from config import PINECONE_API_KEY, PINECONE_ENVIRONMENT, PINECONE_INDEX_NAME, OPENAI_EMBED_MODEL
 
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+def init_pinecone():
+    if not PINECONE_API_KEY:
+        raise RuntimeError("Missing PINECONE_API_KEY")
+    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
 
-def get_pinecone_vectorstore():
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-    return Pinecone.from_existing_index(index_name=PINECONE_INDEX_NAME, embedding=embeddings)
+def create_index_if_not_exists(dim=1536, metric="cosine"):
+    init_pinecone()
+    if PINECONE_INDEX_NAME not in pinecone.list_indexes():
+        pinecone.create_index(name=PINECONE_INDEX_NAME, dimension=dim, metric=metric)
+        return True
+    return False
+
+def get_vectorstore():
+    init_pinecone()
+    embeddings = OpenAIEmbeddings(model=OPENAI_EMBED_MODEL, openai_api_key=None)  # langchain will read env OPENAI_API_KEY
+    return LangPinecone(index_name=PINECONE_INDEX_NAME, embedding=embeddings, pinecone_index=pinecone.Index(PINECONE_INDEX_NAME))
