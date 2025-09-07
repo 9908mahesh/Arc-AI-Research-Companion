d15824890712b7586b3ccb4977a96a78f0b52fba
9908mahesh <maahimahi9908@gmail.com>
2025-09-07 10:46:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,14 +8,13 @@ OPENAI_CHAT_MODEL = os.getenv("OPENAI_CHAT_MODEL", "gpt-4o-mini")
 OPENAI_EMBED_MODEL = os.getenv("OPENAI_EMBED_MODEL", "text-embedding-3-small")
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
-PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT", "us-east-1")
 PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME", "arc-index")
 
 DEFAULT_TOP_K = int(os.getenv("DEFAULT_TOP_K", 5))
 
 def ensure_keys():
     missing = []
-    for k,v in [
+    for k, v in [
         ("OPENAI_API_KEY", OPENAI_API_KEY),
         ("PINECONE_API_KEY", PINECONE_API_KEY),
     ]:
